6ee58c864e6c88f4781f156599c92931093e168a
zwii.doan <zwii.doan@gmail.com>
2019-05-23 17:02:47 +1000

    Async Writes in Redux

---
+++ pluralsight-adminstration/src/components/courses/ManageCoursePage.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import * as authorActions from "../../redux/actions/authorActions";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  saveCourse,
+  history,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({ ...props.errors });
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch(error => alert("Failed to load courses " + error));
+    }
+
+    if (props.course) {
+      setCourse({ ...props.course });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch(error => alert("Failed to load authors " + error));
+    }
+  }, [props.course]);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    setCourse(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
+    }));
+  };
+
+  const handleSave = event => {
+    event.preventDefault();
+    saveCourse(course).then(() => history.push("/courses"));
+  };
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  errors: PropTypes.object,
+  history: PropTypes.object.isRequired
+};
+
+function getCourseBySlug(courses, slug) {
+  return courses.find(course => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course = slug ? getCourseBySlug(state.courses, slug) : newCourse;
+
+  return {
+    courses: state.courses,
+    authors: state.authors,
+    course
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+  saveCourse: courseActions.saveCourse
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageCoursePage);
--- pluralsight-adminstration/src/redux/actions/courseActions.js
+++ pluralsight-adminstration/src/redux/actions/courseActions.js
@@ -1,17 +1,39 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return {
-    type: types.CREATE_COURSE,
-    course
-  };
-}
-
 export function loadCourseSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
     courses
+  };
+}
+
+export function saveCourseSuccess(course) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    course
+  };
+}
+
+export function updateCourseSuccess(course) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    course
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch) {
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(saveCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        throw error;
+      });
   };
 }
 
